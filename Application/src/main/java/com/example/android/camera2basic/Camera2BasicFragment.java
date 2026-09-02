1b154dd67db1830a4b650a208fd100c3ece33ec9
spriter <Albert2020@my.com>
2026-09-02 15:25:09 +0800

    Initial commit

--- Application/src/main/java/com/example/android/camera2basic/Camera2BasicFragment.java
+++ Application/src/main/java/com/example/android/camera2basic/Camera2BasicFragment.java
@@ -44,11 +44,11 @@ import android.media.ImageReader;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.core.content.ContextCompat;
 import android.util.Log;
 import android.util.Size;
 import android.util.SparseIntArray;
@@ -886,20 +886,16 @@ public class Camera2BasicFragment extends Fragment
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.picture: {
-                takePicture();
-                break;
-            }
-            case R.id.info: {
-                Activity activity = getActivity();
-                if (null != activity) {
-                    new AlertDialog.Builder(activity)
-                            .setMessage(R.string.intro_message)
-                            .setPositiveButton(android.R.string.ok, null)
-                            .show();
-                }
-                break;
+        int id = view.getId();
+        if (id == R.id.picture) {
+            takePicture();
+        } else if (id == R.id.info) {
+            Activity activity = getActivity();
+            if (null != activity) {
+                new AlertDialog.Builder(activity)
+                        .setMessage(R.string.intro_message)
+                        .setPositiveButton(android.R.string.ok, null)
+                        .show();
             }
         }
     }
